14117d08c9431398d4eec63d91934361d9fd5a00
youngji <cyj9058@gmail.com>
2017-12-21 14:59:50 +0900

    layout change

--- public/charts.js
+++ public/charts.js
@@ -1,59 +1,57 @@
-google.charts.load('current', {packages: ['corechart']});
+google.charts.load('current', {'packages':['corechart', 'controls']});
 google.charts.setOnLoadCallback(smp_chart);
 google.charts.setOnLoadCallback(rec_chart);
 
 const smp_url= 'https://docs.google.com/spreadsheets/d/1_WuRPqGhuuj7lGJn286_oU35kgUaMZ-fwpEzKART9L8/gviz/tq?sheet=SMP&header=1&range=A1:B200&tq=';
-const rec_url= 'https://docs.google.com/spreadsheets/d/1_WuRPqGhuuj7lGJn286_oU35kgUaMZ-fwpEzKART9L8/gviz/tq?sheet=REC&header=1&range=A1:B12&tq=';
+
+const rec_url= 'https://docs.google.com/spreadsheets/d/1_WuRPqGhuuj7lGJn286_oU35kgUaMZ-fwpEzKART9L8/gviz/tq?sheet=REC&header=1&range=A1:B64&tq=';
 
 function smp_chart(){
   let query = new google.visualization.Query(smp_url);
-  query.send(smp_response);
+  query.send(smp_chartboard);
 }
 
-function smp_response(res){
-  if (res.isError()){
-    alert('Error in query'+res.getMessage()+' '+res.getDetailedMessage());
-    return;
-  }
-  let data=res.getDataTable();
-  let option = {
-    hAxis: {title: '시간'},
-    vAxis: {title: 'SMP 가격'}
-  }
-  let chart= new google.visualization.LineChart(document.getElementById('smp_chart_div'));
-  function selectHandler() {
-    let selectedItem = chart.getSelection()[0];
-    if (selectedItem) {
-      var topping = data.getValue(selectedItem.row, 0);
-      alert('The user selected ' + topping);
+function smp_chartboard(res){
+  let data = res.getDataTable();
+  let time_slider = new google.visualization.ControlWrapper({
+    'controlType': 'DateRangeFilter',
+    'containerId': 'smp_filter_div',
+    'options': {
+        'filterColumnLabel': 'date',
+        'hAxis': {format: 'yy/MM'},
+        'vAxis': {format: 'long'}
     }
-  }
-  chart.draw(data,option);
+  });
+
+  let chart  = new google.visualization.ChartWrapper({
+    'chartType': 'LineChart',
+    'containerId': 'smp_chart_div',
+  });
+  let dashboard = new google.visualization.Dashboard(document.getElementById('smp_dashboard_div'));
+  dashboard.bind(time_slider,chart);
+  dashboard.draw(data)
 }
 
 function rec_chart(){
   let query = new google.visualization.Query(rec_url);
-  query.send(smp_response);
+  query.send(rec_chartboard);
 }
 
-function rec_response(res){
-  if (res.isError()){
-    alert('Error in query'+res.getMessage()+' '+res.getDetailedMessage());
-    return;
-  }
-  let data= res.getDataTable();
-  let option ={
-    hAxis: {title: '시간'},
-    vAxis: {title: 'SMP 가격'}
-  }
-  let chart = new google.visualization.LineChart(document.getElementById('rec_chart_div'));
-  function selectHandler() {
-    let selectedItem = chart.getSelection()[0];
-    if (selectedItem) {
-      var topping = data.getValue(selectedItem.row, 0);
-      alert('The user selected ' + topping);
+function rec_chartboard(res){
+  let data = res.getDataTable();
+  let time_slider = new google.visualization.ControlWrapper({
+    'controlType': 'DateRangeFilter',
+    'containerId': 'rec_filter_div',
+    'options': {
+        'filterColumnLabel': 'date'
     }
-  }
-  google.visualization.events.addListener(chart, 'select', selectHandler);
-  chart.draw(data,option);
+  });
+
+  let chart  = new google.visualization.ChartWrapper({
+    'chartType': 'LineChart',
+    'containerId': 'rec_chart_div',
+  });
+  let dashboard = new google.visualization.Dashboard(document.getElementById('rec_dashboard_div'));
+  dashboard.bind(time_slider,chart);
+  dashboard.draw(data)
 }
