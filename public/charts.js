d1a5bac593bc10d4ee6d4152e58c90a2fb5aa73d
youngji <cyj9058@gmail.com>
2017-12-28 06:10:34 +0900

    차트필터

--- public/charts.js
+++ public/charts.js
@@ -3,11 +3,12 @@ google.charts.setOnLoadCallback(smp_chart);
 google.charts.setOnLoadCallback(rec_chart);
 
 const smp_url= 'https://docs.google.com/spreadsheets/d/1_WuRPqGhuuj7lGJn286_oU35kgUaMZ-fwpEzKART9L8/gviz/tq?sheet=SMP&header=1&range=A1:B200&tq=';
-
 const rec_url= 'https://docs.google.com/spreadsheets/d/1_WuRPqGhuuj7lGJn286_oU35kgUaMZ-fwpEzKART9L8/gviz/tq?sheet=REC&header=1&range=A1:B64&tq=';
 
-function smp_chart(){
-  let query = new google.visualization.Query(smp_url);
+function smp_chart(filter){
+  let filter_url=encodeURIComponent(filter);
+  let query_url=smp_url+filter_url;
+  let query = new google.visualization.Query(query_url);
   query.send(smp_chartboard);
 }
 
@@ -24,8 +25,10 @@ function smp_chartboard(res){
   chart.draw(data,options);
 }
 
-function rec_chart(){
-  let query = new google.visualization.Query(rec_url);
+function rec_chart(filter){
+  let filter_url=encodeURIComponent(filter);
+  let query_url=rec_url+filter_url;
+  let query = new google.visualization.Query(query_url);
   query.send(rec_chartboard);
 }
 
@@ -47,5 +50,16 @@ $(window).resize(function(){
   rec_chart();
 });
 
+$(function(){
+  function smp_filter_func(callback){
+    $('#smp_filter').html('<select name="smp_filter" onChange="smp_chart(this.value)"> <option id="smp_default" value="select A, B where A > date \'2014-7-01\'"> 최근 3개월 </option> <option value="select A, B where A > date \'2016-9-01\'"> 최근 1년 </option> <option value="select A, B where A > date \'2014-9-01\'"> 최근 3년 </option> <option value="select A, B"> 전체 </option></select>');
+    callback();
+  }
+  smp_filter_func(function smp_callback(){
+    smp_chart($("#smp_default").attr("value"));
+  });
+});
 
-document.getElementById("smp_filter_div").innerHTML= "Hello World!"
+$(function(){
+  $('#rec_filter').html('<select name="smp_filter" onChange="rec_chart(this.value)"> <option id="rec_default" value="select A, B where A > date \'2017-3-28\'"> 통합시장이후 </option> <option value="select A, B where A < date \'2017-3-28\'"> 통합시장이전 </option> <option value="select A, B"> 전체 </option></select>');
+});
--- public/charts_filter.js
+++ public/charts_filter.js
@@ -1,6 +1,13 @@
-let smp_filter=document.getElementById("smp_filter_div");
-let rec_filter=document.getElementById("rec_filter_div");
+$(function(){
+  function smp_filter_func(callback){
+    $('#smp_filter').html('<select name="smp_filter" onChange="smp_chart(this.value)"> <option id="smp_default" value="select A, B where A > date \'2014-7-01\'"> 최근 3개월 </option> <option value="select A, B where A > date \'2016-9-01\'"> 최근 1년 </option> <option value="select A, B where A > date \'2014-9-01\'"> 최근 3년 </option> <option value="select A, B"> 전체 </option></select>');
+    callback();
+  }
+  smp_filter_func(function smp_callback(){
+    smp_chart($("#smp_default").attr("value"));
+  });
+});
 
-smp_filter.innerHTML="<select onChange='sendAndDraw(this.value)'> <option value=''>최근 3개월</option><option value='&tq=limit 3'>최근 1년</option><option value='&tq=select G,H'>최근 3년 </option><option value='&tq=select G,H'>전체</option></select>";
-
-rec_filter.innerHTML="<select onChange='sendAndDraw(this.value)'> <option value=''>통합시장이후</option><option value='&tq=limit 3'>통합시장이전</option><option value='&tq=select G,H'>전체</option></select>";
+$(function(){
+  $('#rec_filter').html('<select name="smp_filter" onChange="rec_chart(this.value)"> <option id="rec_default" value="select A, B where A > date \'2017-3-28\'"> 통합시장이후 </option> <option value="select A, B where A < date \'2017-3-28\'"> 통합시장이전 </option> <option value="select A, B"> 전체 </option></select>');
+});
