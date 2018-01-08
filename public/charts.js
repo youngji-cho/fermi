f14b7e44ceb19699c2dd59b7204466ea23f872ff
youngji <cyj9058@gmail.com>
2018-01-08 21:23:36 +0900

    필터 수정

--- public/charts.js
+++ public/charts.js
@@ -2,64 +2,99 @@ google.charts.load('current', {'packages':['corechart', 'controls']});
 google.charts.setOnLoadCallback(smp_chart);
 google.charts.setOnLoadCallback(rec_chart);
 
-const smp_url= 'https://docs.google.com/spreadsheets/d/1_WuRPqGhuuj7lGJn286_oU35kgUaMZ-fwpEzKART9L8/gviz/tq?sheet=SMP&header=1&range=A1:B200&tq=';
+const smp_url= 'https://docs.google.com/spreadsheets/d/1_WuRPqGhuuj7lGJn286_oU35kgUaMZ-fwpEzKART9L8/gviz/tq?sheet=SMP&header=1&range=A1:B201&tq=';
+
 const rec_url= 'https://docs.google.com/spreadsheets/d/1_WuRPqGhuuj7lGJn286_oU35kgUaMZ-fwpEzKART9L8/gviz/tq?sheet=REC&header=1&range=A1:B64&tq=';
 
-function smp_chart(filter){
-  let filter_url=encodeURIComponent(filter);
-  let query_url=smp_url+filter_url;
-  let query = new google.visualization.Query(query_url);
+function smp_chart(){
+  let query = new google.visualization.Query(smp_url);
   query.send(smp_chartboard);
 }
 
 function smp_chartboard(res){
   let data = res.getDataTable();
-  let chart  = new google.visualization.LineChart(document.getElementById('smp_chart_div'));
+  let time_slider = new google.visualization.ControlWrapper({
+    'controlType': 'DateRangeFilter',
+    'containerId': 'smp_filter_div',
+    'options': {
+        'filterColumnLabel': 'date',
+        'hAxis': {format: 'yy/MM'},
+        'vAxis': {format: 'long'}
+    }
+  });
 
-  let options ={
-    'title':'월간 가중평균 계통한계가격 그래프',
-     'legend':{
-       'position':'bottom'
-     }
+  let chart  = new google.visualization.ChartWrapper({
+    'chartType': 'LineChart',
+    'containerId': 'smp_chart_div',
+    'options': {
+      'title':'월간 가중평균 계통한계가격 그래프',
+       'legend':{
+         'position':'bottom'
+       }
+    }
+  });
+  let dashboard = new google.visualization.Dashboard(document.getElementById('smp_dashboard_div'));
+  dashboard.bind(time_slider,chart);
+  dashboard.draw(data);
+
+  smp_1yrs= function() {
+    time_slider.setState({'lowValue': new Date(2016,12,1), 'highValue': new Date(2017,11,1)});
+    time_slider.draw();
   };
-  chart.draw(data,options);
+  smp_3yrs= function(){
+    time_slider.setState({'lowValue': new Date(2014,12,1), 'highValue': new Date(2017,11,1)});
+    time_slider.draw();
+  };
+  smp_all= function(){
+    time_slider.setState({'lowValue': new Date(2001,4,1), 'highValue': new Date(2017,11,1)});
+    time_slider.draw();
+  };
 }
 
-function rec_chart(filter){
-  let filter_url=encodeURIComponent(filter);
-  let query_url=rec_url+filter_url;
-  let query = new google.visualization.Query(query_url);
+function rec_chart(){
+  let query = new google.visualization.Query(rec_url);
   query.send(rec_chartboard);
 }
 
 function rec_chartboard(res){
   let data = res.getDataTable();
-  let chart  = new google.visualization.LineChart(document.getElementById('rec_chart_div'));
+  let time_slider = new google.visualization.ControlWrapper({
+    'controlType': 'DateRangeFilter',
+    'containerId': 'rec_filter_div',
+    'options': {
+        'filterColumnLabel': 'date'
+    },
+  });
 
-  let options ={
-    'title':'REC 도매시장 가격 그래프',
-    'legend':{
-       'position':'bottom'
-     }
+  let chart  = new google.visualization.ChartWrapper({
+    'chartType': 'LineChart',
+    'containerId': 'rec_chart_div',
+    'options':{
+       'title':'REC 도매시장 가격 그래프',
+       'legend':{
+         'position':'bottom'
+       }
+    }
+  });
+  let dashboard = new google.visualization.Dashboard(document.getElementById('rec_dashboard_div'));
+  dashboard.bind(time_slider,chart);
+  dashboard.draw(data);
+
+  rec_after= function() {
+    time_slider.setState({'lowValue': new Date(2017,3,28), 'highValue': new Date(2017,11,16)});
+    time_slider.draw();
   };
-  chart.draw(data,options);
+  rec_before= function(){
+    time_slider.setState({'lowValue': new Date(2014,12,1), 'highValue': new Date(2013,3,26)});
+    time_slider.draw();
+  };
+  rec_all= function(){
+    time_slider.setState({'lowValue': new Date(2017,3,28), 'highValue': new Date(2017,11,16)});
+    time_slider.draw();
+  };
 }
 
 $(window).resize(function(){
   smp_chart();
   rec_chart();
-});
-
-$(function(){
-  function smp_filter_func(callback){
-    $('#smp_filter').html('<select name="smp_filter" onChange="smp_chart(this.value)"> <option id="smp_default" value="select A, B where A > date \'2014-7-01\'"> 최근 3개월 </option> <option value="select A, B where A > date \'2016-9-01\'"> 최근 1년 </option> <option value="select A, B where A > date \'2014-9-01\'"> 최근 3년 </option> <option value="select A, B"> 전체 </option></select>');
-    callback();
-  }
-  smp_filter_func(function smp_callback(){
-    smp_chart($("#smp_default").attr("value"));
-  });
-});
-
-$(function(){
-  $('#rec_filter').html('<select name="smp_filter" onChange="rec_chart(this.value)"> <option id="rec_default" value="select A, B where A > date \'2017-3-28\'"> 통합시장이후 </option> <option value="select A, B where A < date \'2017-3-28\'"> 통합시장이전 </option> <option value="select A, B"> 전체 </option></select>');
 });
