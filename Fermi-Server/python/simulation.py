d32890396fc0cd93326befc31579aec2ba45fb49
youngji <cyj9058@gmail.com>
2018-04-09 14:35:44 +0900

    python 모델 수정

--- Fermi-Server/python/simulation.py
+++ Fermi-Server/python/simulation.py
@@ -4,6 +4,9 @@ import numpy as np
 import pandas as pd
 import json
 import os
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+import datetime
 
 config_path=os.path.abspath(os.path.join(__file__ ,"../../.."))
 config=json.load(open(os.path.join(config_path,'config.json')))
@@ -30,6 +33,29 @@ data=pd.DataFrame(data=array[:,1:6],index=array[:,0],columns=["smp_price","oil_p
 data.smp_price=data.smp_price*data.korea_producer
 data.oil_price=data.oil_price*data.us_producer
 data['date']= data.index
+
+data=data.astype(float)
+
+# 회귀분석 모델 만들기
+model=smf.ols(formula = 'smp_price~oil_price+elec_supply', data = data).fit()
+
+def make_scenario(year,scenario):
+    curs=conn.cursor()
+    fields_sql="SHOW FIELDS FROM `oil_forecast`"
+    curs.execute(fields_sql)
+    fields=[]
+    fields_result=curs.fetchall()
+    for i in fields_result:
+        fields.append(i[0])
+    time=pd.period_range(start=datetime.date.today().strftime("%Y-%m-%d"),periods=12*15,freq='M')
+    startDate=str(time[0])
+    endDate=str(time[len(time)-1])
+    main_sql ="SELECT * FROM `oil_forecast` where date>=%s && date<=%s"
+    curs=conn.cursor()
+    curs.execute(main_sql,(startDate+'-01',endDate+'-01'))
+    result=pd.DataFrame(data=np.array(curs.fetchall()),index=time,columns=fields).loc[:,scenario]
+    return result
+
 
 def main(input):
     export_data=input.to_json(orient='records',date_format="iso")
