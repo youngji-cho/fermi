5d4b6e5ea32dfc944d8a50aad93b212668b14954
youngji <cyj9058@gmail.com>
2018-04-24 16:30:34 +0900

    https다시

--- Fermi-Server/main.js
+++ Fermi-Server/main.js
@@ -10,13 +10,19 @@ const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS
 if(process.env.NODE_ENV=='production'){
   console.log("Production Mode")
   process.env.PORT=3000
-  app.use(redirectToHTTPS());
+  app.use(function(req, res, next) {
+    if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https')) {
+        res.redirect('https://' + req.get('Host') + req.url);
+    }
+    else
+        next();
+    });
 } else if(process.env.NODE_ENV=='development'){
   console.log("Development Mode")
+  app.use(cors());
   process.env.PORT=4000
 }
 
-app.use(cors());
 app.use('/economic',economic.router);
 app.use('/energy_data',energy_data.router);
 app.use('/',express.static(path.resolve(__dirname, './../Fermi-Client')));
